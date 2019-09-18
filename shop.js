51e8dc73df73abf07ec4cd6cc0938c498b21a118
Markus Markowich <markus902@gmx.de>
2019-09-17 22:59:50 -0400

    added manager view question

--- shop.js
+++ shop.js
@@ -6,7 +6,6 @@ var database;
 var shoppingCart = [];
 var quantities = [];
 
-read();
 
 function addProductToCart(product, quantity) {
     shoppingCart.push(database[product]);
@@ -80,7 +79,6 @@ function buyProduct() {
                     choices: ['Yes', 'No']
                 }]).then(function (choice) {
                     if (choice.anotherProduct === 'Yes') {
-                        console.log(shoppingCart, quantities)
                         buyProduct();
                     } else {
                         checkout();
@@ -102,10 +100,8 @@ function read() {
         password: "Helmar@123",
         database: "bamazon"
     })
-
     connection.connect(function (err) {
         if (err) throw err;
-
     });
     connection.query("SELECT * from products", function (err, res) {
         if (err) throw err;
@@ -118,11 +114,18 @@ function read() {
         })
         console.log(table.toString());
         database = res;
-
-        // console.log(database)
-
-
-        buyProduct();
+        inquirer.prompt([{
+            message: 'Who are you?',
+            name: 'manager',
+            type: 'list',
+            choices: ['Manager', 'Customer']
+        }]).then(function (manager) {
+            if (manager.choices === 'yes') {
+                manager();
+            } else {
+                buyProduct();
+            }
+        });
     })
 
 }
@@ -131,26 +134,18 @@ function read() {
 
 
 
-// Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-// The app should then prompt users with two messages.
+read();
 
 
-
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
+// Create a new Node application called bamazonManager.js. Running this application will:
 
 
-
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
-
-
-// However, if your store does have enough of the product, you should fulfill the customer's order.
-
-
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
+// List a set of menu options:
+// View Products for Sale
+// View Low Inventory
+// Add to Inventory
+// Add New Product
+// If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
+// If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
+// If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+// If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
